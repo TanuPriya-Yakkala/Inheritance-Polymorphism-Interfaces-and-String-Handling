f85eb7beaeb7e049f399a4f85f706c6acfae7184
TanuPriya-Yakkala <69051004+TanuPriya-Yakkala@users.noreply.github.com>
2021-10-09 21:12:30 +0530

    Create StringHandling.java

---
+++ StringHandling.java
@@ -0,0 +1,7 @@
+public class StringHandling {
+   public static void main(String args[]) {
+      char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
+      String helloString = new String(helloArray);
+      System.out.println( helloString );
+   }
+}
